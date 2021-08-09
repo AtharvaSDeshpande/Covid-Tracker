0efe8628d96c7fb9cdd1d2d7e66b3ad68c8f7728
Atharva <atharvadeshpande174@gmail.com>
2021-08-09 21:17:07 +0530

    Build

---
+++ build/precache-manifest.45ffd4d707b1fb8657d8bad85461e21a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0cb36ed959231248b6b02c5f87bbfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19ec14d3d42f387c4598",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "28f55ae1d0b5bc83becb",
+    "url": "/static/css/main.2468ff9b.chunk.css"
+  },
+  {
+    "revision": "19ec14d3d42f387c4598",
+    "url": "/static/js/2.fba65abe.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.fba65abe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28f55ae1d0b5bc83becb",
+    "url": "/static/js/main.207c72d6.chunk.js"
+  },
+  {
+    "revision": "a3180c9a53571a03a367",
+    "url": "/static/js/runtime-main.5cef0218.js"
+  }
+]);
